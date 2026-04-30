568ff7e95da9bc65c5911050c8b889a9e5e4c390
younes1819 <younes.berrahal@alu.ceacfp.es>
2026-04-30 03:50:33 +0200

    Delete js directory

--- js/constants.js
+++
@@ -1,5 +0,0 @@
-/** Longitud máxima permitida para el título de una tarea (caracteres). */
-export const MAX_TASK_TITLE_LENGTH = 200;
-
-/** Longitud mínima tras normalizar espacios. */
-export const MIN_TASK_TITLE_LENGTH = 1;
